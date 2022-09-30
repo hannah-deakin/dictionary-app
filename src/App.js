48bcf43b5c76c743fe6b195b20f1761ee196f4c7
hannah-deakin <hannah.v.deakin@gmail.com>
2022-09-30 14:23:37 +0100

    Added Dictionary component and changed the styling of the App header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+          <p>
+            "If the word doesn't exist, invent it; but first be sure it doesn't
+            exist"
+            <br /> – Charles Baudelaire
+          </p>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Hannah Deakin</footer>
+      </div>
     </div>
   );
 }
