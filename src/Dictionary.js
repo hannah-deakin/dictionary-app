87eac511dcbf351bf9eb236a4f8063f465bcc754
hannah-deakin <hannah.v.deakin@gmail.com>
2022-10-03 13:37:57 +0100

    Added meaning component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results.js";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("null");
+  let [results, setResults] = useState("null");
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    //console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -25,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
