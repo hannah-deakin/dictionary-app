ef12c106ad8c75dabc769f4b081a767436ba572b
hannah-deakin <hannah.v.deakin@gmail.com>
2022-10-08 13:40:01 +0100

    Added Definitions component and added if/else statement to Definitions

---
+++ src/Definitions.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Definitions(props) {
+  return (
+    <div className="Definitions">
+      <strong>Definitions: </strong>
+      {props.definitions}
+    </div>
+  );
+}
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Definitions from "./Definitions.js";
 import Examples from "./Examples.js";
 import Synonyms from "./Synonyms.js";
 
@@ -7,16 +8,19 @@ export default function Meaning(props) {
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              <strong>Definition:</strong> {definition.definition}
-              <br />
-              <Examples examples={definition.example} />
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
-          </div>
-        );
+        if (index <= 4) {
+          return (
+            <div key={index}>
+              <p>
+                <Definitions definitions={definition.definition} />
+                <Examples examples={definition.example} />
+                <Synonyms synonyms={definition.synonyms} />
+              </p>
+            </div>
+          );
+        } else {
+          return null;
+        }
       })}
     </div>
   );
