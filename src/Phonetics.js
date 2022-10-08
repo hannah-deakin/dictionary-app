190d0fd01ff97b832c70117144efe66e30872038
hannah-deakin <hannah.v.deakin@gmail.com>
2022-10-08 12:52:02 +0100

    Removed console logs, added Phonetic and Phonetics components

---
+++ src/Phonetics.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Phonetic from "./Phonetic.js";
+
+export default function Phonetics(props) {
+  return (
+    <div className="Phonetics">
+      {props.phonetics.map(function (phonetic, index) {
+        if (index === 1) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Meaning from "./Meaning.js";
+import Phonetics from "./Phonetics.js";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <Phonetics phonetics={props.results.phonetics} />
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
